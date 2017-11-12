20c5baca884247680f20c4dfd84ee58e4572f3fa
yu li <Auther@yus-MacBook-Pro.local>
2017-11-12 15:01:31 -0500

    finished q3

--- A2/q2_3.py
+++ A2/q2_3.py
@@ -23,15 +23,27 @@ def compute_parameters(train_data, train_labels):
     where the ith row corresponds to the ith digit class.
     '''
     eta = np.zeros((10, 64))
+    for i in range(train_data.shape[0]):
+        for j in range(64):
+            if train_data[i][j] == 1:
+                eta[int(train_labels[i])][j] += 1
+
+    eta = (eta +1)/train_data.shape[0]*10
+
     return eta
 
 def plot_images(class_images):
     '''
     Plot each of the images corresponding to each class side by side in grayscale
     '''
+    image = []
     for i in range(10):
         img_i = class_images[i]
-        # ...
+        image.append(img_i.reshape(8,8))
+
+    all_concat = np.concatenate(image, 1)
+    plt.imshow(all_concat, cmap='gray')
+    plt.show()
 
 def generate_new_data(eta):
     '''
@@ -41,6 +53,9 @@ def generate_new_data(eta):
     Plot these values
     '''
     generated_data = np.zeros((10, 64))
+    for i in range(eta.shape[0]):
+        for j in range(eta.shape[1]):
+            generated_data[i][j] = np.random.binomial(1,eta[i][j])
     plot_images(generated_data)
 
 def generative_likelihood(bin_digits, eta):
@@ -50,7 +65,16 @@ def generative_likelihood(bin_digits, eta):
 
     Should return an n x 10 numpy array 
     '''
-    return None
+    g_lh = np.zeros((bin_digits.shape[0],10))
+    for i in range(bin_digits.shape[0]):
+        for k in range(10):
+            temp = 1
+            for j in range(64):
+                first = eta[k][j]**bin_digits[i][j]
+                sec = (1-eta[k][j])**(1-bin_digits[i][j])
+                temp = temp*first*sec
+            g_lh[i][k] = temp
+    return np.log(g_lh)
 
 def conditional_likelihood(bin_digits, eta):
     '''
@@ -61,7 +85,10 @@ def conditional_likelihood(bin_digits, eta):
     This should be a numpy array of shape (n, 10)
     Where n is the number of datapoints and 10 corresponds to each digit class
     '''
-    return None
+    gl = np.exp(generative_likelihood(bin_digits, eta))
+    p_y = 0.1
+    p_x = np.sum(gl * p_y, axis=1).reshape(-1, 1)
+    return np.log(gl) + np.log(p_y) - np.log(p_x)
 
 def avg_conditional_likelihood(bin_digits, labels, eta):
     '''
@@ -72,9 +99,12 @@ def avg_conditional_likelihood(bin_digits, labels, eta):
     i.e. the average log likelihood that the model assigns to the correct class label
     '''
     cond_likelihood = conditional_likelihood(bin_digits, eta)
-
+    total = 0
     # Compute as described above and return
-    return None
+    for i in range(bin_digits.shape[0]):
+        correct_class = labels[i]
+        total += cond_likelihood[i][int(correct_class)]
+    return total / bin_digits.shape[0]
 
 def classify_data(bin_digits, eta):
     '''
@@ -82,7 +112,18 @@ def classify_data(bin_digits, eta):
     '''
     cond_likelihood = conditional_likelihood(bin_digits, eta)
     # Compute and return the most likely class
-    pass
+    classes = np.zeros((bin_digits.shape[0], 1))
+    for i in range(bin_digits.shape[0]):
+        classes[i] = np.where(cond_likelihood[i] == cond_likelihood[i].max())
+    return classes
+
+
+def eval_accuracy(predicted_labels, true_labels):
+    match = 0
+    for i in range(predicted_labels.shape[0]):
+        if int(predicted_labels[i])== int(true_labels[i]):
+            match+=1
+    return match/predicted_labels.shape[0]
 
 def main():
     train_data, train_labels, test_data, test_labels = data.load_all_data('data')
@@ -90,11 +131,20 @@ def main():
 
     # Fit the model
     eta = compute_parameters(train_data, train_labels)
-
+    #print(eta)
     # Evaluation
     plot_images(eta)
-
     generate_new_data(eta)
+    avg_train_clh = avg_conditional_likelihood(train_data,train_labels, eta)
+    avg_test_clh = avg_conditional_likelihood(test_data, test_labels, eta)
+    print("train: %s test: %s" %(np.exp(avg_test_clh), np.exp(avg_train_clh)))
+    train_classes = classify_data(train_data,eta)
+    train_accuracy = eval_accuracy(train_classes, train_labels)
+    test_classes = classify_data(test_data, eta)
+    test_accuracy = eval_accuracy(test_classes, test_labels)
+
+    print("train_accruracy: %s test_accuract: %s" %(train_accuracy, test_accuracy))
+
 
 if __name__ == '__main__':
     main()
